d260d702b63a7ceddbe1b072b6aa7bf120601a2f
“CraftyVlad“ <vladsemboss@gmail.com>
2024-12-19 20:21:11 +0200

    Completed tasks

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// Завдання 3
+// Створіть і запустіть додаток React, який виведе у браузер інформацію про вашу улюблену книгу. Наприклад: назву книги, ПІБ автора, жанр книги, кількість сторінок і декілька рецензій до книги.
+// Під час розробки використовуйте функціональні компоненти та синтаксис JSX.
+// Завдання 4
+// Реалізуйте додаток з попереднього завдання з використанням класових компонентів.
+
+class BookDetails extends React.Component {
+	render() {
+		const { title, author, genre, pages, reviews } = this.props.book;
+
+		return (
+			<div>
+				<h1>My favorite book</h1>
+				<img src="book.jpg" alt="" />
+				<h2>{title}</h2>
+				<p>Author: {author}</p>
+				<p>Genre: {genre}</p>
+				<p>Amount of pages: {pages}</p>
+				<h2>Reviews:</h2>
+				<div>
+					{reviews.map((review, index) => (
+						<p>
+							{review.reviewer}: *{review.rating}* {review.comment}
+						</p>
+					))}
+				</div>
+			</div>
+		);
+	}
+}
+
+class App extends React.Component {
+	state = {
+		book: {
+			title: "Климко",
+			author: "Григір Михайлович Тютюнник",
+			genre: "Повість",
+			pages: 158,
+			reviews: [
+				{
+					reviewer: "Артем",
+					rating: 4,
+					comment: "Книга сподобалась.",
+				},
+				{
+					reviewer: "Микола",
+					rating: 2,
+					comment: "Сумна книга!",
+				},
+				{
+					reviewer: "Яна",
+					rating: 3,
+					comment: "Коли буде кіно?",
+				},
+			],
+		},
+	};
+
+	render() {
+		return (
+			<div className="App">
+				<BookDetails book={this.state.book} />
+			</div>
+		);
+	}
 }
 
 export default App;
